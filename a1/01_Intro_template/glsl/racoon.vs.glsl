f37359381d3983e1b86b727e1e50131b4be3c07d
ericchen321 <cgx1997@outlook.com>
2020-09-26 20:07:04 -0700

    refactored 1d a bit

--- a1/01_Intro_template/glsl/racoon.vs.glsl
+++ a1/01_Intro_template/glsl/racoon.vs.glsl
@@ -11,18 +11,17 @@ void main() {
 
     // Make paws same scale as the remote orb
     float scale = 7.0;
-    mat4 scaleMatrix = modelMatrix;
+    mat4 scaleMatrix = mat4(1.0);
     scaleMatrix[0][0] = scale;
     scaleMatrix[1][1] = scale;
     scaleMatrix[2][2] = scale;
 
     // Compute position in world frame
-    vec4 position_world = scaleMatrix * vec4(position, 1.0);
-    vec4 a = modelMatrix * position_world;
+    vec4 position_world = modelMatrix * scaleMatrix * vec4(position, 1.0);
 
     // Compute distance from vertex to remote's centre
-    interpolatedDist = distance(a, vec4(remotePosition, 1.0));
+    interpolatedDist = distance(position_world, vec4(remotePosition, 1.0));
 
     // Multiply each vertex by the model-view matrix and the projection matrix to get final vertex position
-    gl_Position = projectionMatrix * modelViewMatrix * position_world;
+    gl_Position = projectionMatrix * viewMatrix * position_world;
 }
