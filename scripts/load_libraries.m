900e50ba712617dae47ae1e7193b2bf26164f2f4
ericchen321 <cgx1997@outlook.com>
2020-11-14 23:28:16 -0800

    script to load all library functions to workspace

--- scripts/load_libraries.m
+++ scripts/load_libraries.m
@@ -2,5 +2,12 @@
 
 source("bar/bar_interpolate.m")
 source("bar/bar_solve_bar_coords.m")
-source("bezier/bezier_solve_cubic_handles.m")
+source("bezier/bezier_solve_pos_quadratic.m")
+source("bezier/bezier_solve_pos_cubic.m")
+source("bezier/bezier_solve_handles_cubic.m")
 source("clip/clip_solve_plane_edge_intersection.m")
+source("clip/clip_solve_plane_edge_intersection.m")
+source("ray_trace/ray_trace_solve_intersection_sphere.m")
+source("ray_trace/ray_trace_solve_intersection_plane.m")
+source("ray_trace/ray_trace_solve_intersection_triangle.m")
+
