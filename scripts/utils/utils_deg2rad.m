f220d3642f872f622693e4b66d0ad01e327554b0
ericchen321 <cgx1997@outlook.com>
2020-11-28 18:20:38 -0800

    deg & rad conversions

---
+++ scripts/utils/utils_deg2rad.m
@@ -0,0 +1,6 @@
+# converts an angle from deg to rad
+
+function rad = utils_deg2rad (deg)
+  rad = deg/180.0 * pi;
+  return;
+endfunction
---
+++ scripts/utils/utils_rad2deg.m
@@ -0,0 +1,6 @@
+# convert an angle from radians to degs
+
+function deg = utils_rad2deg (rad)
+  deg = rad * 180.0 / pi;
+  return;
+endfunction
