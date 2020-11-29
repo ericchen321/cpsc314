b63600d22c840afe5cb4f8966fd9a27db24f0abc
ericchen321 <cgx1997@outlook.com>
2020-11-28 18:19:39 -0800

    made episilon fraction error

--- scripts/utils/utils_check_equal.m
+++ scripts/utils/utils_check_equal.m
@@ -1,12 +1,12 @@
-# checks if the given value is within the range of [check_val-epsilon, check_val+epsilon]
+# checks if the given value is within the range of [check_val*(1-epsilon), check_val*(1+epsilon)]
 # given_val: value for comparision
 # check_val: value to be compared against
-# epsilon: half of error range
+# epsilon: half of error range expressed as fraction
 # returns true if equal, false if not
 
 function retval = utils_check_equal (given_val, check_val, epsilon)
   retval = false;
-  if(given_val >= check_val-epsilon && given_val<=check_val+epsilon)
+  if(given_val >= check_val*(1-epsilon) && given_val<=check_val*(1+epsilon))
     retval = true;
   endif
   return;
